cb107b5c391041333d4a5007f76b3e0308dba72c
ottiliastudios <info@ottiliastudios.com>
2025-06-15 22:01:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ with cols[1]:  # Mittlere Spalte
 
 
 # --- Description ---
-st.markdown('<div class="description-text">Upload a photo of your old piece of jewelry. Our AI estimates the weight and suggests matching new designs!</div>', unsafe_allow_html=True)
+st.markdown('<div class="description-text">Upload a photo of your old piece of jewelry next to a ruler. Our AI estimates the weight and suggests matching new designs!</div>', unsafe_allow_html=True)
 
 st.markdown("""
 <div class="external-button-small">
